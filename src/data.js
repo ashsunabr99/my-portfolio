d804b2b52534425073511b7a33ab30f5e1c1eb8f
Ashish Sunny Abraham <ashsunabr99@gmail.com>
2024-01-23 15:10:16 -0500

    images changes commit

--- src/data.js
+++ src/data.js
@@ -3,15 +3,15 @@ export const projects = [
     title: "Twitter Engine Clone",
     subtitle: "Distributed OS",
     description:
-      "A twitter engine implemented using erlang actor model and paired with cowboy WebSocket framework which simulates all the basic twitter functions.",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+      "A twitter engine implemented using erlang actor model and paired with JSON based API clone which integrates all design features and functionalities of Twitter using Erlang’s Cowboy web framework. All Twitter Features are implemented.",
+    image: "./twitter_clone.jpg",
+    link: "https://github.com/ashsunabr99/Twitter_replica_4.2",
   },
   {
     title: "Log data Visualizer",
     subtitle: "Tableau and NLP",
     description:
-      "This software enables users to analyze textual log data, exploring an analyst's interactions to understand their thought process and behavior.",
+    "This software enables users to analyze textual log data, exploring an analyst's interactions to understand their thought process and behavior. Using React and Tableau, this interactive dashboard preprocesses data using natural language processing.",
     image: "./Log Data Visualizer.gif",
     link: "https://info-viz-project.w3spaces.com/",
   },
@@ -20,16 +20,32 @@ export const projects = [
     subtitle: "Spring Boot Framework, React JS, Axios, MySQL",
     description:
       "A virtual space management system, implemented in micro-service architecture where the marketing staff of a mall can book spaces online for sellers.",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+    image: "./emall.gif",
+    link: "https://github.com/ashsunabr99/web-emall",
   },
   {
     title: "Sky Detection Algorithm",
     subtitle: "Python and Image Processing",
     description:
       "A complete implementation of a sky region detection algorithm within a single image using python, image processing, and energy optimization function.",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+    image: "./skydetect.png",
+    link: "https://github.com/ashsunabr99",
+  },
+  {
+    title: "Maternal Health Risk Prediction Model",
+    subtitle: "Feature Selection · Artificial Intelligence (AI)",
+    description:
+      "Built a feature selection project which leverages Internet of Medical Things (IoMT) device data for maternal health risk prediction. Through diverse feature selection techniques, a crucial subset of features is identified to improve risk assessment.",
+    image: "./Maternal.png",
+    link: "https://github.com/ashsunabr99/FeatureSelection_MHR",
+  },
+  {
+    title: "BITCOIN Mining",
+    subtitle: "Erlang · Actor Model",
+    description:
+      "A bitcoin mining algorithm which uses Erlang and actor model to implement cryptographic hashing for mining bitcoins. A bitcoin is an input that, when “hashed” produces an output smaller than a target value.",
+    image: "./bitcoin.jpg",
+    link: "https://github.com/ashsunabr99",
   },
 ];
 
